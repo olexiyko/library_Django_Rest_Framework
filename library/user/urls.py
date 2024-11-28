aee63f09effaa7d5949105cd5d48962f9c30a45c
Olexiy Shumylo <you@example.com>
2024-11-28 16:52:41 +0200

    Implement view for special user(with id)

--- library/user/urls.py
+++ library/user/urls.py
@@ -4,4 +4,5 @@ from .views import UserAPIView
 
 urlpatterns = [
     path('',UserAPIView.as_view()),
+    path('<int:id>/',UserAPIView.as_view())
 ]
--- library/user/views.py
+++ library/user/views.py
@@ -4,14 +4,23 @@ from .models import User
 from rest_framework.response import Response
 from .serializers import UserSerializator
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class UserAPIView(APIView):
-    def get(self,request):
-        users=User.objects.filter().all()
-        if users:
-            serialized_users=UserSerializator(users,many=True)
-            return Response(serialized_users.data,status=status.HTTP_200_OK)
-        else: 
-            return Response({"error": "User not found"},status=status.HTTP_404_NOT_FOUND) 
-        
+    def get(self,request,id=None):
+        if id:
+            # user=get_object_or_404(User,pk=id)
+            user=User.objects.filter(pk=id).first()
+            if user:
+                serialized_user=UserSerializator(user)
+                return Response(serialized_user.data,status=status.HTTP_200_OK)
+            return Response({"error": "User not found"},status=status.HTTP_404_NOT_FOUND)          
+        else:
+            users=User.objects.filter().all()
+            if users:
+                serialized_users=UserSerializator(users,many=True)
+                return Response(serialized_users.data,status=status.HTTP_200_OK)
+            else: 
+                return Response({"error": "Users not found"},status=status.HTTP_404_NOT_FOUND) 
+            
