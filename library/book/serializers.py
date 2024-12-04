90f1836add3b208498968c0ddc8b262c7deb3b32
olexiyko <oleksiykoshumylo@gmail.com>
2024-12-04 17:46:28 +0200

    update serializator for book

--- library/book/serializers.py
+++ library/book/serializers.py
@@ -1,41 +1,53 @@
-from rest_framework import serializers # type: ignore
+from rest_framework import serializers  # type: ignore
 from .models import Book
 from author.serializers import AuthorSerializator
 from author.models import Author
 
 
 class BookSerializator(serializers.ModelSerializer):
-    authors = AuthorSerializator(many=True, read_only=True) 
+    authors = AuthorSerializator(many=True, read_only=True)
     author_ids = serializers.ListField(
         child=serializers.IntegerField(), write_only=True, required=False
-    ) 
+    )
+
     class Meta:
         model = Book
-        fields = ['id', 'title', 'description', 'count', 'authors', 'author_ids']
+        fields = ["id", "title", "description", "count", "authors", "author_ids"]
 
     def create(self, validated_data):
-        author_ids = validated_data.pop('author_ids', [])
+        author_ids = validated_data.pop("author_ids", [])
         book = Book.objects.create(**validated_data)
 
         if author_ids:
             existing_authors = Author.objects.filter(id__in=author_ids)
-            existing_author_ids = existing_authors.values_list('id', flat=True)
+            existing_author_ids = existing_authors.values_list("id", flat=True)
+            print(existing_authors.values_list("id", flat=True))
 
             new_author_ids = set(author_ids) - set(existing_author_ids)
             for author_id in new_author_ids:
-                Author.objects.create(id=author_id, name="Default Name", surname="Default Surname")
+                Author.objects.create(
+                    id=author_id, name="Default Name", surname="Default Surname"
+                )
 
             authors = Author.objects.filter(id__in=author_ids)
             book.authors.set(authors)
 
         return book
 
-    def update(self,instance,validated_data):
-        author_ids = validated_data.pop('author_ids', [])
+    def update(self, instance, validated_data):
+        author_ids = validated_data.pop("author_ids", [])
         if author_ids:
+            existing_authors = Author.objects.filter(id__in=author_ids)
+            existing_author_ids = existing_authors.values_list("id", flat=True)
+
+            new_author_ids = set(author_ids) - set(existing_author_ids)
+            for author_id in new_author_ids:
+                Author.objects.create(
+                    pk=author_id, name="Default Name", surname="Default Surname"
+                )
             authors = Author.objects.filter(id__in=author_ids)
             instance.authors.set(authors)
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
-        instance.save()  
+        instance.save()
         return instance
