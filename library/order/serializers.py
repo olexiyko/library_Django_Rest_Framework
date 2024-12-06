509301365853fa622d29653b87ee4e7aaf2f07d3
olexiyko <oleksiykoshumylo@gmail.com>
2024-12-06 20:03:02 +0200

    Implement method put and delete for Order viewset

--- library/order/serializers.py
+++ library/order/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Order
 
+
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Order
-        fields='__all__'
+        model = Order
+        fields = "__all__"
+        extra_kwargs = {"book": {"required": False}, "user": {"required": False}}
--- library/order/views.py
+++ library/order/views.py
@@ -35,3 +35,40 @@ class OrderApiView(APIView):
                 status=status.HTTP_201_CREATED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id=None):
+        if id:
+            order = Order.objects.filter(pk=id).first()
+            if order:
+                order.delete()
+                return Response(
+                    {"success": "Order is successfuly deleted"},
+                    status=status.HTTP_200_OK,
+                )
+            return Response(
+                {"error": "Orders not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        return Response(
+            {"error": "Id id required to delete order"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    def put(self, request, id=None):
+        if id:
+            order = Order.objects.filter(pk=id).first()
+            if order:
+                serializer = OrderSerializer(instance=order, data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(
+                        {"success": "Order is successfuly updated"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Orders not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        return Response(
+            {"error": "Id id required to update order"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
